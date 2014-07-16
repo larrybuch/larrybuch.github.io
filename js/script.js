6e30aaf85ea3208a17029ab148d1d70b047f6b1e
Larry Buchanan <larrybuch@gmail.com>
2014-07-16 00:44:48 -0400

    better

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function() { init(); };
 
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0ApL2ZVhpOmONdDVqZ2lQb0UxbUs0YW1MXzV2N19uaFE&output=html';
+var nestedData;
 
 function init() {
   Tabletop.init( { key: public_spreadsheet_url,
@@ -9,16 +10,50 @@ function init() {
 }
 
 function showInfo(data, tabletop) {
-  console.log(data);
   visualizeIt(data);
 }
 
 function visualizeIt(data){
-  var wrapper = d3.select("#the-goods .second");
+  // var wrapper = d3.select("#the-goods .second");
 
-  // refactor this so rows are being wrapped in links, not the other way around
-  var links = wrapper.selectAll(".link")
-    .data(data)
+  // // refactor this so rows are being wrapped in links, not the other way around
+  // var links = wrapper.selectAll(".link")
+  //   .data(data)
+  //   .enter()
+  //   .append("a")
+  //   .attr("href", function(d){ return d.url; })
+  //   .attr("target", "_blank");
+
+  // var rows = links.append("div")
+  //   .attr("class", function(d){ return d.tag + " row"; })
+  //   .text(function(d){ return d.headline; });
+
+  // var wit = rows.append("span")
+  //   .text(function(d){if (d.wit) {return " with " + d.wit;} });
+
+  nestedData = d3.nest()
+    .key(function(d){ return d.tag; })
+    .entries(data);
+
+  // trying a second thing
+  var wrap = d3.select("body");
+
+  var sections = wrap.selectAll(".test")
+    .data(nestedData)
+    .enter()
+    .append("section")
+    .attr("class", "chunk")
+    .attr("id", function(d){ return d.key.replace(/\s+/g, '-').toLowerCase(); });
+
+  var sectionTitle = sections.append("div")
+    .attr("class", "first")
+    .text(function(d){ return d.key; });
+
+  var secondSection = sections.append("div")
+    .attr("class", "second");
+
+  var links = secondSection.selectAll(".link")
+    .data(function(d){ return d.values; })
     .enter()
     .append("a")
     .attr("href", function(d){ return d.url; })
@@ -30,6 +65,12 @@ function visualizeIt(data){
 
   var wit = rows.append("span")
     .text(function(d){if (d.wit) {return " with " + d.wit;} });
+
+  console.log(nestedData);
+
+  // var body = d3.select('body')
+  //   .append("section")
+  //   .attr("id", "")
 
 }
 
