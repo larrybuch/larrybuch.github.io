d214d13693f585251a2d9bf8b88d142468f8ec63
Larry Buchanan <larrybuch@gmail.com>
2017-04-05 00:30:33 -0400

    new

--- js/script.js
+++ js/script.js
@@ -15,40 +15,60 @@ function showInfo(data, tabletop) {
 
 function visualizeIt(data){
 
-  nestedData = d3.nest()
-    .key(function(d){ return d.tag; })
-    .entries(data);
-
-  // trying a second thing
-  var wrap = d3.select("body");
-
-  var sections = wrap.selectAll(".test")
-    .data(nestedData)
+  var list = d3.select("#links");
+  
+  var link = list.selectAll(".link")
+    .data(data)
     .enter()
-    .append("section")
-    .attr("class", "chunk")
-    .attr("id", function(d){ return d.key.replace(/\s+/g, '-').toLowerCase(); });
+    .append("div")
+    .append("p");
+    
+    link
+      .append("span")
+      .attr("class", "date")
+      .html(function(d){ return d.pubdate; });
 
-  var sectionTitle = sections.append("div")
-    .attr("class", "first")
-    .text(function(d){ return d.key; });
+    link.append("a")
+      .html(function(d){ return d.headline; })
+      .attr("href", function(d){ return d.url; })
+      .attr("target", "_blank");
 
-  var secondSection = sections.append("div")
-    .attr("class", "second");
 
-  var links = secondSection.selectAll(".link")
-    .data(function(d){ return d.values; })
-    .enter()
-    .append("a")
-    .attr("href", function(d){ return d.url; })
-    .attr("target", "_blank");
 
-  var rows = links.append("div")
-    .attr("class", function(d){ return d.tag + " row"; })
-    .text(function(d){ return d.headline; });
+  // nestedData = d3.nest()
+  //   .key(function(d){ return d.tag; })
+  //   .entries(data);
 
-  var wit = rows.append("span")
-    .text(function(d){if (d.wit) {return " with " + d.wit;} });
+  // // trying a second thing
+  // var wrap = d3.select("body");
+
+  // var sections = wrap.selectAll(".test")
+  //   .data(nestedData)
+  //   .enter()
+  //   .append("section")
+  //   .attr("class", "chunk")
+  //   .attr("id", function(d){ return d.key.replace(/\s+/g, '-').toLowerCase(); });
+
+  // var sectionTitle = sections.append("div")
+  //   .attr("class", "first")
+  //   .text(function(d){ return d.key; });
+
+  // var secondSection = sections.append("div")
+  //   .attr("class", "second");
+
+  // var links = secondSection.selectAll(".link")
+  //   .data(function(d){ return d.values; })
+  //   .enter()
+  //   .append("a")
+  //   .attr("href", function(d){ return d.url; })
+  //   .attr("target", "_blank");
+
+  // var rows = links.append("div")
+  //   .attr("class", function(d){ return d.tag + " row"; })
+  //   .text(function(d){ return d.headline; });
+
+  // var wit = rows.append("span")
+  //   .text(function(d){if (d.wit) {return " with " + d.wit;} });
 
   console.log("I have no idea what I'm doing.");
 
